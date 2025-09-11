502324419817eaf2f8985a38091e41ec270bb077
Rezamrs22 <rezamrs22@gmail.com>
2025-09-11 17:21:50 +0200

    Repo på rot-nivå: inkluderar Server + Client

---
+++ Lagerorder1.Client/Program.cs
@@ -0,0 +1,20 @@
+using Lagerorder1.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Lagerorder1.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
